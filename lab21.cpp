4b76e87acf969add3af840821e81a70057b0ad94
Oleg K <dock290@gmail.com>
2022-03-21 10:51:54 +0400

    Update lab21

--- lab21.cpp
+++ lab21.cpp
@@ -1,46 +1,18 @@
 #include <MultiFunctionShield.h>
 
-#define LATCH_DIO 4
-#define CLK_DIO 7
-#define DATA_DIO 8
-
-// typedef char byte;
-
-// Number codes to print digit on mfs' display
-const byte SEGMENT_MAP[] = {
-    0xC0,  // 0
-    0xF9,  // 1
-    0xA4,  // 2
-    0xB0,  // 3
-    0x99,  // 4
-    0x92,  // 5
-    0x82,  // 6
-    0xF8,  // 7
-    0X80,  // 8
-    0X90,  // 9
-};
-
-// Codes to select mfs' display
-const byte SEGMENT_SELECT[] = {0xF1, 0xF2, 0xF4, 0xF8};
+MultiFunctionShield MFS;
 
 void setup() {
-  uint8_t i;
-
-  MultiFunctionShield MFS;
   MFS.begin();
 
-  // pinMode(PIN_BUTTON, INPUT);
-
-  pinMode(LATCH_DIO, OUTPUT);
-  pinMode(CLK_DIO, OUTPUT);
-  pinMode(DATA_DIO, OUTPUT);
+  pinMode(BUTTON_1_PIN, INPUT);
 }
 
 bool manualMode = false;
 int printMode = 0;
 
 void loop() {
-  if (printMode >= 4) {
+  if (printMode >= 3) {
     printMode = 0;
   }
 
@@ -53,9 +25,9 @@ void loop() {
 
 void handleInitialMode() {
   if (!digitalRead(BUTTON_1_PIN)) {
-    digitalWrite(BUTTON_1_PIN, LOW);
+    digitalWrite(BUZZER_PIN, LOW);
     delay(20);
-    digitalWrite(BUTTON_1_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, HIGH);
     delay(500);
 
     manualMode = true;
@@ -65,18 +37,19 @@ void handleInitialMode() {
   }
 
   printNumber(printMode);
+  
   printMode++;
 }
 
 void handleManualMode() {
   if (!digitalRead(BUTTON_1_PIN)) {
-    digitalWrite(BUTTON_1_PIN, LOW);
+    digitalWrite(BUZZER_PIN, LOW);
     delay(20);
-    digitalWrite(BUTTON_1_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, HIGH);
     delay(500);
 
     printMode++;
-    if (printMode >= 4) {
+    if (printMode >= 3) {
       printMode = 0;
     }
   }
@@ -87,28 +60,27 @@ void handleManualMode() {
 void printNumber(int printMode) {
   switch (printMode) {
     case 0: {
-      printNumberToDisplay(0, 0);
-      printNumberToDisplay(1, 5);
+      MFS.Display(0);
+      delay(500);
+      MFS.Display(5);
+      delay(500);
     } break;
 
     case 1: {
-      printNumberToDisplay(0, 0);
-      printNumberToDisplay(1, 8);
+      MFS.Display(0);
+      delay(500);
+      MFS.Display(8);
+      delay(500);
     } break;
 
     case 2: {
-      printNumberToDisplay(0, 1);
-      printNumberToDisplay(1, 0);
+      MFS.Display(1);
+      delay(500);
+      MFS.Display(0);
+      delay(500);
     } break;
 
     default:
       break;
   }
-}
-
-void printNumberToDisplay(byte display, byte number) {
-  digitalWrite(LATCH_DIO, LOW);
-  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_MAP[number]);
-  shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[display]);
-  digitalWrite(LATCH_DIO, HIGH);
 }
