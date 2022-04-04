b6f5b3ef335b479344d32338d6b7d6a1eecaa2c9
dock290 <dock290@gmail.com>
2022-04-04 12:21:51 +0400

    Update lab22

--- lab22.cpp
+++ lab22.cpp
@@ -1,5 +1,5 @@
-#include <MultiFunctionShield.h>
 #include <Arduino_FreeRTOS.h>
+#include <MultiFunctionShield.h>
 
 MultiFunctionShield MFS;
 
@@ -14,162 +14,72 @@ void setup() {
 
   pinMode(BUTTON_1_PIN, INPUT);
 
-  xTaskCreate(TaskPrintFirst,
-              "PrintFirst",
-              128,
-              NULL,
-              0,
-              &tasks[0]
-             );
-             
-    xTaskCreate(TaskPrintSecond,
-                "PrintSecond",
-                128,
-                NULL,
-                0,
-              &tasks[1]
-      );
-    vTaskSuspend(tasks[1]);
-    
-    xTaskCreate(TaskPrintThird,
-                "PrintThird",
-                128,
-                NULL,
-                0,
-              &tasks[2]
-      );
-    vTaskSuspend(tasks[2]);
-    
-    xTaskCreate(TaskPrintManual,
-                "PrintManual",
-                128,
-                NULL,
-                0,
-              &taskManual
-      );
-    vTaskSuspend(taskManual);
+  xTaskCreate(TaskPrintFirst, "PrintFirst", 128, NULL, 0, &tasks[0]);
+
+  xTaskCreate(TaskPrintSecond, "PrintSecond", 128, NULL, 0, &tasks[1]);
+  vTaskSuspend(tasks[1]);
+
+  xTaskCreate(TaskPrintThird, "PrintThird", 128, NULL, 0, &tasks[2]);
+  vTaskSuspend(tasks[2]);
+
+  xTaskCreate(TaskPrintManual, "PrintManual", 128, NULL, 0, &taskManual);
+  vTaskSuspend(taskManual);
 }
 
+void loop() {}
 
-void loop () {
-}
-
-// 0 - Not pressed
-// 1 - Pressed
-// 2 - Was read
-int updateButtonMode() {
-  int buttonPressed = 0;
-  // If not pressed or if was read
-  if (buttonPressed == 0 || buttonPressed == 2) {
-    int newButtonPressed = !digitalRead(BUTTON_1_PIN);
-    if (newButtonPressed == 1 && buttonPressed == 0) {
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(20);
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(500);
-    
-      buttonPressed = 1;
-    }
-  }
-  return buttonPressed;
+void noize() {
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(20);
+  digitalWrite(BUZZER_PIN, HIGH);
 }
 
 bool handleInitialMode(int printMode) {
-  int buttonPressed = updateButtonMode();
-  
-  if (buttonPressed == 1) {
-    printMode = 1;
-    return true;
-  }
+  printNumber(printMode);
 
-  if (Serial.available() > 0) {
-    if (Serial.read() == 'q') {
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(20);
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(500);
-      printMode = 1;
+  bool buttonHold = false;
+  long startTime = xTaskGetTickCount();
+  while (true) {
+    bool button1Pressed = !digitalRead(BUTTON_1_PIN);
+    if (button1Pressed && !buttonHold) {
       return true;
+      buttonHold = true;
+    } else if (!button1Pressed) {
+      buttonHold = false;
+    }
+
+    if (Serial.available() > 0) {
+      if (Serial.read() == 'q') {
+        noize();
+        printMode = 1;
+        return true;
+      }
+    }
+
+    long currentTime = xTaskGetTickCount();
+    long elapsedTime = currentTime - startTime;
+    if (elapsedTime > 100) {
+      return false;
     }
   }
-
-  if (buttonPressed == 1) {
-    buttonPressed = 2;
-  }
-
-  printNumber(printMode);
   return false;
-}
-
-int handleManualMode(int printMode) {
-  int buttonPressed = updateButtonMode();
-  if (buttonPressed) {
-    return printMode + 1;
-  }
-  
-  if (Serial.available() > 0) {
-    char ch = Serial.read();
-    if (ch == '1') {
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(20);
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(500);
-      return 0;
-    } else if (ch == '2') {
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(20);
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(500);
-      return 1;
-    } else if (ch == '3') {
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(20);
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(500);
-      return 2;
-    } else if (ch == 'q') {
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(20);
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(500);
-      return printMode + 1;
-    }
-  }
-
-  printNumber(printMode);
-  return printMode;
 }
 
 void printNumber(int printMode) {
   switch (printMode) {
     case 0: {
-        Serial.println(5, DEC);
-        MFS.Display(0);
-        delay(500);
-        MFS.Display(5);
-        delay(500);
-      } break;
+      Serial.println(5, DEC);
+      MFS.Display(05);
+    } break;
 
     case 1: {
-        Serial.println(8, DEC);
-        MFS.Display(0);
-        delay(500);
-        MFS.Display(8);
-        delay(500);
-      } break;
+      Serial.println(8, DEC);
+      MFS.Display(8);
+    } break;
 
     case 2: {
-        Serial.println(10, DEC);
-        MFS.Display(1);
-        delay(500);
-        MFS.Display(0);
-        delay(500);
-      } break;
-
-    default: {
-        Serial.println("printNumber() wrong param");
-        MFS.Display(1111);
-        delay(500);
+      Serial.println(10, DEC);
+      MFS.Display(10);
     } break;
   }
 }
@@ -215,11 +125,55 @@ void TaskPrintThird(void *pvParams) {
 
 void TaskPrintManual(void *pvParams) {
   int printMode = 0;
+  bool buttonHold = false;
+  long startTime = xTaskGetTickCount();
   while (true) {
-    printMode = handleManualMode(printMode);
-    
+    bool button1Pressed = !digitalRead(BUTTON_1_PIN);
+    if (button1Pressed && !buttonHold) {
+      printMode++;
       if (printMode > 2) {
         printMode = 0;
       }
+      printNumber(printMode);
+      buttonHold = true;
+    } else if (!button1Pressed) {
+      buttonHold = false;
+    }
+
+    if (Serial.available() > 0) {
+      char ch = Serial.read();
+      if (ch == '1') {
+        noize();
+        printMode = 0;
+      } else if (ch == '2') {
+        noize();
+        printMode = 1;
+      } else if (ch == '3') {
+        noize();
+        printMode = 2;
+      } else if (ch == 'q') {
+        noize();
+        printMode++;
+      } else if (ch == '0') {
+        noize();
+        printMode = -1;
+      }
+    }
+
+    if (printMode > 2) {
+      printMode = 0;
+    }
+
+    long currentTime = xTaskGetTickCount();
+    long elapsedTime = currentTime - startTime;
+    if (elapsedTime > 100) {
+      printNumber(printMode);
+      startTime = currentTime;
+    }
+
+    if (printMode == -1) {
+      vTaskResume(tasks[0]);
+      vTaskSuspend(taskManual);
+    }
   }
 }
